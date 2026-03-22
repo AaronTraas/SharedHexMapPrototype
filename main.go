a717316436bd9901df51ce6ab70d64ddec461bc2
Aaron Traas <aaron@traas.org>
2026-03-21 20:54:26 -0400

    Get endpoint working

--- main.go
+++ main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
   "net/http"
 )
+
+type GridCell struct {
+  Id       string `json:"id"`
+  Contents string `json:"contents"`
+}
+
+type Response struct {
+  Message  string   `json:"message"`
+  Status   int      `json:"status"`
+  GridCell GridCell `json:"grid-cell"`
+}
 
 func main() {
   // Define a route and handler for serving static files at the root
   fs := http.FileServer(http.Dir("./static"))
   http.Handle("/", http.StripPrefix("/", fs))
 
-  // Define a route and handler for services
-  http.HandleFunc("/service", func(w http.ResponseWriter, r *http.Request) {
-      fmt.Fprintln(w, "Welcome to my Go web service!")
+  // Define a route and handler for load endpoint
+  http.HandleFunc("/api/load", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+
+    cell := GridCell{
+      Id: "default",
+      Contents: "Hello World",
+    }
+
+    res := Response{
+      Message: "Success",
+      Status: 200,
+      GridCell: cell,
+    }
+
+    json.NewEncoder(w).Encode(res)
+  })
+
+  // Define a route and handler for save endpoint
+  http.HandleFunc("/api/save", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+
+    res := Response{
+      Message: "Success",
+      Status: 200,
+    }
+
+    json.NewEncoder(w).Encode(res)
   })
 
   // Start the server on port 8080
--- static/script.js
+++ static/script.js
@@ -1,4 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   console.debug('Script loaded')
-  //const hamburger_menu = document.getElementById('sidebar')
+  const load_button = document.getElementById('load-button')
+  const save_button = document.getElementById('save-button')
+  const grid_id = document.getElementById('form-grid-id')
+  const grid_value = document.getElementById('form-grid-value')
+  const response_output = document.getElementById('response')
+
+  const load_handler = async function() {
+    try {
+      const response = await fetch('/api/load');
+      const data = await response.json();
+
+      const json_string = JSON.stringify(data, null, 2)
+
+      console.debug(`Response: ${json_string}`);
+      response_output.innerHTML = json_string;
+
+      grid_id.value = data['grid-cell']['id']
+      grid_value.value = data['grid-cell']['contents']
+    } catch(e) {
+      console.error(e)
+    }
+  }
+
+  save_button.addEventListener('click', function() {
+    console.debug('Save button was clicked!');
+  });
+
+  load_button.addEventListener('click', function() {
+    console.debug('Load button was clicked!');
+    load_handler()
+  });
+
+  load_handler();
 })
